89602ba2a8e8a3938ead382b2caa78a99b1f2191
Zack Barovsky <57148341+zbarovsky@users.noreply.github.com>
2021-12-11 13:58:52 -0800

    initial commit

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface FooterProps {
+    
+}
+
+const Footer: React.FC<FooterProps> = () => {
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface NavbarProps {
+
+}
+
+const Navbar: React.FC<NavbarProps> = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Navbar;
